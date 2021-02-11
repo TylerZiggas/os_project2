f0a384ac34e28216a9a3660f04fdcaa5181130b5
Tyler Ziggas <tyler@hoare7.cs.umsl.edu>
2021-02-11 15:46:01 -0600

    Made file parsing

--- master.c
+++ master.c
@@ -4,14 +4,16 @@
 #include <getopt.h>
 #include <string.h>
 #include <errno.h>
+#include <sys/wait.h>
 
 int main (int argc, char *argv[]) {
-	int character, numChild = 20, timeSec = 100;
+	int character, numChild = 20, timeSec = 100, count = 0, items = 0, temp;
+	FILE* datafile;
 	while ((character = getopt(argc, argv, "s:t:h")) != -1) {
 		switch (character) {
 			case 's':
 				printf("test s");
-				if (isdigit(*optarg)) {
+				if (isdigit(*optarg) && *optarg <= 20) {
 					numChild = *optarg;
 				}
 				continue;
@@ -30,6 +32,28 @@ int main (int argc, char *argv[]) {
 				break;
 		}
 	}
+	datafile = fopen(argv[2], "r");
+	if (!datafile) {
+		printf("Problem with the datafile.");
+		return 0;
+	}
+
+	while(fscanf(datafile, "%d", &temp) == 1) {
+		items++;
+	}
+
+	if (items % 2 == 1) {
+		items++;
+	}	
+
+	printf("%d\n", items);
+	int intArray[items];
+	intArray[items] = 0;	
+	rewind(datafile);
+
+	while(count < items && fscanf(datafile, "%d", &intArray[count]) == 1) {
+		count++;
+	} 
 	return 0;
 }
 
